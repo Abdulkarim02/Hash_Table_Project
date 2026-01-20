9be436d634e0bbcadc46ab819036a103689450f6
Abdulkarim <444101988@student.ksu.edu.sa>
2026-01-20 18:41:30 +0300

    c

---
+++ src/hash_table.h
@@ -0,0 +1,15 @@
+#ifndef HASH_TABLE_H
+#define HASH_TABLE_H
+
+#include<stdio.h>
+
+void* search(void *a, void *v);
+
+void insert(void *a, void *k, void *v);
+
+void deletes(void *a, void *k);
+
+
+
+
+#endif
